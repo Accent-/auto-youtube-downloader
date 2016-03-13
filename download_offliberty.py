5d8cc5b34925d5b57f6b3ee214e4c2fc476163da
accent <start.world.end@gmail.com>
2016-03-14 02:16:44 +0900

    modify download_offliberty.py

--- download_offliberty.py
+++ download_offliberty.py
@@ -13,12 +13,10 @@ def start_firefox():
 	
 	return webdriver.Firefox(profile)
 
-video = []
+video_link = []
 with open("./youtube_list.txt", 'r') as f:
 	for l in f:
-		video.append(l.replace("\n", ""))
-
-video_link = video
+		video_link.append(l.replace("\n", ""))
 
 driver = start_firefox()
 
